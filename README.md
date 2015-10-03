d5e73209c6ab16e8839169457835af3019687b7d
SALLES Stéphane <gsmurf7@gmail.com>
2015-10-04 00:47:41 +0200

    Appel des informations velib fonctionnel

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # iotVelib
+
+TODO LIST
+===
+
+* mettre en place des appels aux api avec des promises pour recevoir les informations dans l'ordre et pouvoir faire des traitements correctement comme par exemple les informations de trajet
--- config.dist.js
+++ config.dist.js
@@ -1,7 +1,14 @@
 module.exports = {
-  key: '',
-  stations_check: [{number: 15109,
-                   name: 'CEVENNES',
-                   alias: 'Home'
-                   }]
+  JCDecaux: {
+    api_key: '',
+    contract_name: 'Paris',
+    stations_check: [{number: 15109,
+                     name: 'CEVENNES',
+                     alias: 'Home'
+                     }]
+  },
+  Parrot : {
+    api_key: ''
+  }
+  
 };
