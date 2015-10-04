d32f34cd0e29693f1757fe220c25b25ebfc0c194
SALLES Stéphane <gsmurf7@gmail.com>
2015-10-04 02:18:47 +0200

    Promises en place et premier check de trajet fonctionnel

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 TODO LIST
 ===
 
-* mettre en place des appels aux api avec des promises pour recevoir les informations dans l'ordre et pouvoir faire des traitements correctement comme par exemple les informations de trajet
+* ~~mettre en place des appels aux api avec des promises pour recevoir les informations dans l'ordre et pouvoir faire des traitements correctement comme par exemple les informations de trajet~~
+* ~~faire fonctionner les checks de trajet~~
+* afficher un délai pour les infos de derniere mise à jour d'une station, plutôt que la date d'update
--- velib.js
+++ velib.js
@@ -12,23 +12,22 @@ var Station = function(st){
 };
 
 var Velib = {
-    getInfo: function(idStation, affiche){
-        request.get({url:'https://api.jcdecaux.com/vls/v1/stations/'+idStation+'?contract='+config.contract_name+'&apiKey='+config.api_key,
-            contract:config.contract_name,
-            json:true}, function (e, r, st) {
-                if (st.error == 'Unauthorized') {
-                    console.warn("Action non-autorisée, votre clé API Jc Decaux n'est surement pas valide.\nVous avez saisi : "+config.api_key);
-                    return false;
-                } else{
-                    var station = new Station(st);
-                    if (affiche === undefined) {
-                        Velib.afficheStation(station);
-                    }else{
-                        return station;
+    getInfo: function(idStation) {
+        var API = new Promise(function(resolve, reject) {
+            request.get({url:'https://api.jcdecaux.com/vls/v1/stations/'+idStation+'?contract='+config.contract_name+'&apiKey='+config.api_key,
+                contract:config.contract_name,
+                json:true}, function (e, r, st) {
+                    if (st.error == 'Unauthorized') {
+                        reject("Action non-autorisée, votre clé API Jc Decaux n'est surement pas valide.\nVous avez saisi : "+config.api_key);
+                    } else{
+                        var station = new Station(st);
+                        resolve(station);
                     };
-                };
-            }
-        );
+                }
+            );
+        });
+
+        return API;
     },
     afficheStation: function(station){
         if(station.statut == 'CLOSED'){
@@ -40,31 +39,67 @@ var Velib = {
             console.log('Dernière maj : '+station.update.toTimeString())
         }
     },
-    checkTrajet: function(depart, arrivee){
-        var depart = Velib.getInfo(id_depart);
-        var arrivee = Velib.getInfo(id_arrivee);
-        var error = false;
-        var message = [];
-        
-        if (depart.status == CLOSED) {
-            var error = true;
-            message.push('La station de départ '+depart.name.substr(8, depart.name.length)+' est fermée');
-        };
-        if (arrivee.status == CLOSED) {
-            var error = true;
-            message.push('La station d\'arrivée '+arrivee.name.substr(8, arrivee.name.length)+' est fermée');
-        };
-        if (depart.available_bikes < 1) {
-            var error = true;
-            message.push('Il n\'y a pas de vélib disponible');
-        };
-        if (arrivee.available_bike_stands < 1) {
-            var error = true;
-            message.push('Il n\'y a pas de point d\'attache disponible');
-        };
+    checkTrajet: function(idDepart, idArrivee, label){
+        Velib.getInfo(idDepart).then(function(depart) { // 15109 : CEVENNES Home
+            Velib.getInfo(idArrivee).then(function(arrivee) { // 15032 : LOURMEL M 8
+                var error = false;
+                var message = [];
+                
+                if (depart.statut == 'CLOSED') {
+                var error = true;
+                message.push("La station de départ '+depart.nom+' est fermée");
+                };
+                if (arrivee.statut == 'CLOSED') {
+                var error = true;
+                message.push("La station d'arrivée '+arrivee.nom+' est fermée");
+                };
+                if (depart.velos < 1) {
+                var error = true;
+                message.push("Il n'y a pas de vélib disponible");
+                };
+                if (arrivee.places < 1) {
+                var error = true;
+                message.push("Il n'y a pas de point d'attache disponible");
+                };
+
+                // Affichage du trajet
+                console.info(label);
+                if (error) {
+                    console.warn(message);
+                } else{
+                    console.info(depart.velos+' velo(s) t\'attendent à '+depart.nom+', pour '+arrivee.places+'  de libre à '+arrivee.nom+' !');
+                };
+
+            }, function(reason) {
+              console.log(reason); // Error!
+            });
+        }, function(reason) {
+          console.log(reason); // Error!
+        });
     }
 };
 
-Velib.getInfo(15109); // 15109 : CEVENNES Home
-Velib.getInfo(15032); // 15032 : LOURMEL M 8
-Velib.getInfo(15064); // 15064 : JAVEL M 10
+Velib.checkTrajet(15109, 15032, "pour M8");
+Velib.checkTrajet(15109, 15064, "pour M10");
+
+Velib.checkTrajet(15032, 15109, "retour homy du M8");
+Velib.checkTrajet(15064, 15109, "retour homy du M10");
+
+// affichage des stations
+/*
+Velib.getInfo(15109).then(function(value) { // 15109 : CEVENNES Home
+  Velib.afficheStation(value);
+    Velib.getInfo(15032).then(function(value) { // 15032 : LOURMEL M 8
+      Velib.afficheStation(value);
+        Velib.getInfo(15064).then(function(value) { // 15064 : JAVEL M 10
+          Velib.afficheStation(value);
+        }, function(reason) {
+          console.log(reason); // Error!
+        });
+    }, function(reason) {
+      console.log(reason); // Error!
+    });
+}, function(reason) {
+  console.log(reason); // Error!
+});
+*/
