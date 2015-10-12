0cd168730abf9354a7d6e67dba915b36ea91d259
Stéphane SALLES <s.salles@sicavonline.fr>
2015-10-12 14:23:56 +0200

    Ajout d'un second trajet de retour pour le test

--- main.js
+++ main.js
@@ -4,9 +4,14 @@ var Velib = require('./lib/velib');
 var trajets = [];
 var velib = new Velib();
 
-velib.checkTrajet(15064, 15109, "retour homy du M10").then(function(trajet){
+velib.checkTrajet(15062, 15109, "retour homy du M8").then(function(trajet){
     trajets.push(trajet);
-    afficheLesTrajets(trajets);
+    velib.checkTrajet(15064, 15109, "retour homy du M10").then(function(trajet){
+    	trajets.push(trajet);
+	afficheLesTrajets(trajets);
+    }, function(reason){
+        afficheLesTrajets();
+    });
 }, function(reason){
     afficheLesTrajets();
 });
